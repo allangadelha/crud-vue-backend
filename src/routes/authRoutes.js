043b0beb5cd94e1dfc0a04aecb9692b4d3c11239
allangadelha <allangadelha@gmail.com>
2022-07-10 10:13:03 -0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const { db } = require('../database/db');
 const { authController } = require('../controllers/authController');
 const authRoutes = express();
 
